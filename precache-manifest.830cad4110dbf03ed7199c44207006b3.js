8c5b609d4a9acab8cd95b3630cebb5933ed6e303
Sherlock <sherlock@sherlooks-MacBook-Pro.local>
2020-11-22 09:21:55 +0330

    Updates

---
+++ precache-manifest.830cad4110dbf03ed7199c44207006b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c30783b3436a5fc1c5b8d24051f01c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b07b70bf74a1d18e4cfe",
+    "url": "/monsters-rolodex/static/css/main.338373ad.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b07b70bf74a1d18e4cfe",
+    "url": "/monsters-rolodex/static/js/main.4988587a.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
